558f40ff705279da5fff1db508bba0d5cd28d6c6
AlfredFranciss <alfred.francis1999@gmail.com>
2021-05-26 16:55:31 -0400

    Main module added

---
+++ main.py
@@ -0,0 +1,7 @@
+import areaofcircle
+import revname
+if __name__ == '__main__':
+    print('< - - - - - A R E A  O F  C I R C L E - - - - - >\n')
+    areaofcircle.getradius()
+    print('\n< - - - - - R E V E R S E   N A M E - - - - - - >\n')
+    revname.getname()
